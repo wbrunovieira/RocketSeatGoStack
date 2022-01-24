818966f1ea72d57af28b9c14770011f632900744
wbrunovieira <wbrunovieira77@gmail.com>
2022-01-24 18:38:05 +0000

    first react-native project

--- mobile/index.js
+++ mobile/index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
+i
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './src';
+
 
 AppRegistry.registerComponent(appName, () => App);
